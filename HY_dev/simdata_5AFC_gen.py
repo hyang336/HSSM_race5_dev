fc2a0cc55f3f6573aa6c961177533b45cddd1ab0
Haopei Yang <hyang336@gra-login3.graham.sharcnet>
2024-02-12 17:40:46 -0500

    started coding for race_no_bias_5 training data generation

--- HY_dev/simdata_5AFC_gen.py
+++ HY_dev/simdata_5AFC_gen.py
@@ -1,31 +1,17 @@
-from ssms.basic_simulators import simulator
-import numpy as np
+import ssms
 import pandas as pd
-a_true_sample=np.random.normal(loc=2,scale=0.3,size=1000)
-v_true_sample=np.random.normal(loc=2,scale=0.3,size=1000)
-z_true_sample=np.random.normal(loc=2,scale=0.3,size=1000)
-t_true_sample=np.random.normal(loc=2,scale=0.3,size=1000)
+import numpy as np
+from ssms.basic_simulators.simulator import simulator
+from copy import deepcopy
 
-par_mat = np.zeros((1000, 4))
-par_mat[:, 0] = v_true_sample
-par_mat[:, 1] = a_true_sample
-par_mat[:, 2] = z_true_sample
-par_mat[:, 3] = t_true_sample
+#set up the generator and model configs
+generator_config = deepcopy(ssms.config.data_generator_config['lan'])
+generator_config['dgp_list'] = 'race_no_bias_5'
+generator_config['output_folder'] = '/scratch/hyang336/working_dir/HSSM_dev/race_no_bias_5_dev/'
 
-# Simulate data
-sim_out_trialwise = simulator.simulator(
-    theta=par_mat,  # parameter_matrix
-    model="ddm",  # specify model (many are included in ssms)
-    n_samples=1,  # number of samples for each set of parameters
-)
+model_config = ssms.config.model_config['race_no_bias_5']
 
-# Turn into nice dataset
-dataset_trialwise = pd.DataFrame(
-    np.column_stack(
-        [sim_out_trialwise["rts"][:, 0], sim_out_trialwise["choices"][:, 0]]
-    ),
-    columns=["rt", "response"],
-)
-
-dataset_trialwise
-
+#get generator
+my_generator = ssms.dataset_generators.lan_mlp.data_generator(generator_config = generator_config, model_config = model_config)
+#generate data
+training_data = my_generator.generate_data_training_uniform(save=True)
